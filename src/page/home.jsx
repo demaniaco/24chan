51fbf69260058ef70a6c047d81c2eb6c6df51931
Ruben Zagon <ruben.zagon@gmail.com>
2019-12-16 20:28:21 +0000

    Pagina fucking evar guapens

---
+++ src/page/home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Boards from "../components/boards/boards";
+
+export const HomePage = () => {
+  return (
+    <div>
+      <Boards></Boards>
+    </div>
+  );
+};
